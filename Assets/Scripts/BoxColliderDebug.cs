1f1aaecfaf1aad9bdad8c0a80e28c6ba3fafcb08
3 <dannyli0109@gmail.ccom>
2021-07-04 13:50:02 +1000

    added animator

---
+++ Assets/Scripts/BoxColliderDebug.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxColliderDebug : MonoBehaviour
+{
+    LineRenderer lineRenderer;
+    BoxCollider2D boxCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 boxSize = boxCollider.size;
+        Vector3 p1 = new Vector3(transform.position.x - boxSize.x / 2, transform.position.y + boxSize.y / 2, 0);
+        Vector3 p2 = new Vector3(transform.position.x + boxSize.x / 2, transform.position.y + boxSize.y / 2, 0);
+        Vector3 p3 = new Vector3(transform.position.x + boxSize.x / 2, transform.position.y - boxSize.y / 2, 0);
+        Vector3 p4 = new Vector3(transform.position.x - boxSize.x / 2, transform.position.y - boxSize.y / 2, 0);
+
+
+        // Debug.DrawLine
+
+        lineRenderer.SetPosition(0, p1);
+        lineRenderer.SetPosition(1, p2);
+        // lineRenderer.SetPosition(2, p3);
+        // lineRenderer.SetPosition(3, p4);
+    }
+}
---
+++ Assets/Scripts/PlayerInputManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputManager : MonoBehaviour
+{
+    public Vector3 mousePos;
+    public Vector3 mouseWorldPos;
+    public Vector3 playerPos;
+    public Camera camera;
+    public GameObject model;
+    public Animator playerAnimator;
+
+    LineRenderer lineRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Input.mousePosition;
+        // mousePos.y = mousePos.y;
+        mousePos.z = camera.nearClipPlane;
+
+
+        mouseWorldPos = camera.ScreenToWorldPoint(mousePos);
+
+        playerPos = transform.position;
+        
+        //playerPos.z = camera.nearClipPlane;
+        //playerPos.y = mouseWorldPos.y;
+
+        Vector3 lookDir = mousePos - playerPos;
+        lineRenderer.SetPosition(0, playerPos);
+        lineRenderer.SetPosition(1, mouseWorldPos);
+
+
+        float yRotation = Quaternion.LookRotation(lookDir, Vector3.up).eulerAngles.y;
+
+        //atan2(v.y, v.x) * 180.0f / M_PI;
+
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x);
+        float deg = Mathf.Rad2Deg * angle;
+        //Debug.Log(deg);
+
+        Vector3 eulerAngle = transform.rotation.eulerAngles;
+        eulerAngle.y = deg;
+        model.transform.rotation = Quaternion.Euler(eulerAngle);
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        playerAnimator.SetFloat("x", x);
+        playerAnimator.SetFloat("y", y);
+
+        // string str = "x: " + x + "y: " + y;
+        // Debug.Log(str);
+
+    }
+}
