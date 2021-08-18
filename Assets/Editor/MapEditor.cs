84f41aafeaeb5f6ed3ed06158e527c3c8ff12fa2
3 <dannyli0109@gmail.ccom>
2021-08-18 12:16:38 +1000

    added new fonts

--- Assets/Editor/MapEditor.cs
+++ Assets/Editor/MapEditor.cs
@@ -1,22 +1,22 @@
 ﻿using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(MapGenerator))]
-public class MapEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
+//[CustomEditor(typeof(MapGenerator))]
+//public class MapEditor : Editor
+//{
+//    public override void OnInspectorGUI()
+//    {
+//        base.OnInspectorGUI();
 
-        MapGenerator mapGenerator = (MapGenerator)target;
-        if (GUILayout.Button("Generate Object"))
-        {
-            mapGenerator.GenerateMap();
-        }
+//        MapGenerator mapGenerator = (MapGenerator)target;
+//        if (GUILayout.Button("Generate Object"))
+//        {
+//            mapGenerator.GenerateMap();
+//        }
 
-        if (GUILayout.Button("Clear tiles"))
-        {
-            mapGenerator.ClearTileMap();
-        }
-    }
-}
+//        if (GUILayout.Button("Clear tiles"))
+//        {
+//            mapGenerator.ClearTileMap();
+//        }
+//    }
+//}
