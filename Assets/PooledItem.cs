b12aa28d85d03527035de2fc400f5134a0bbfa3d
3 <dannyli0109@gmail.ccom>
2021-09-09 10:09:43 +1000

    added back bst manager to player

---
+++ Assets/PooledItem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool<T> where T : PooledItem
+{
+    Queue<T> available;
+
+    public Pool(T prefab, int count)
+    {
+        available = new Queue<T>();
+        for (int i = 0; i < count; i++)
+        {
+            T entity = GameObject.Instantiate(prefab);
+            entity.gameObject.SetActive(false);
+            entity.onDestroy += (x) => available.Enqueue(x as T);
+            available.Enqueue(entity);
+        }
+    }
+
+    public bool TryInstantiate(out T instantiateEntity, Vector3 position, Quaternion rotation)
+    {
+        if (available.Count > 0)
+        {
+            instantiateEntity = available.Dequeue();
+            instantiateEntity.transform.SetPositionAndRotation(position, rotation);
+            instantiateEntity.gameObject.SetActive(true);
+            return true;
+        }
+        instantiateEntity = null;
+        return false;
+    }
+}
+
+public class PooledItem : MonoBehaviour
+{
+    public event System.Action<PooledItem> onDestroy;
+
+    protected void ReturnToPool()
+    {
+        gameObject.SetActive(false);
+        onDestroy?.Invoke(this);
+    }
+}
