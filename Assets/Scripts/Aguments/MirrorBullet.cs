459ae3cb2c7ff0f06a66e1719b17e85c5498189e
3 <dannyli93@gmail.com>
2021-10-07 15:36:06 +1100

    added mirror augment

---
+++ Assets/Scripts/Aguments/MirrorBullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Augments/MirrorBullet")]
+public class MirrorBullet : Augment
+{
+    public override void Init()
+    {
+        InitEvents();
+    }
+
+    public override void Shoot(Character character, Transform transform)
+    {
+        if (index == 0) return;
+        character.inventory[index - 1].index = index - 1;
+        character.inventory[index - 1].Shoot(character, transform);
+    }
+}
