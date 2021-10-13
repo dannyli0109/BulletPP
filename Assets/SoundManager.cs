1ceaeab129c5cea78da5b9349eed3d7a8e1ebc03
3 <dannyli93@gmail.com>
2021-10-14 04:12:08 +1100

    added fmod and play shooting sounds

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public enum SoundType
 {
     FootStep,
@@ -13,38 +12,33 @@ public class SoundManager : MonoBehaviour
 {
     public static SoundManager current;
     public SoundPool soundPool;
-
+    
     [Range(0, 1)]
     public float volume;
 
-    public Dictionary<SoundType, AudioClip> audioMap = new Dictionary<SoundType, AudioClip>();
+    //public Dictionary<SoundType, AudioClip> audioMap = new Dictionary<SoundType, AudioClip>();
+    public Dictionary<SoundType, string> audioMap = new Dictionary<SoundType, string>();
     private void Awake()
     {
         current = this;
 
-        audioMap.Add(SoundType.FootStep, Resources.Load<AudioClip>("Sounds/FootStep"));
-        audioMap.Add(SoundType.Gunshot, Resources.Load<AudioClip>("Sounds/Gunshot"));
-        audioMap.Add(SoundType.GunshotEnemy, Resources.Load<AudioClip>("Sounds/GunshotEnemy"));
+        //audioMap.Add(SoundType.FootStep, Resources.Load<AudioClip>("Sounds/FootStep"));
+        //audioMap.Add(SoundType.Gunshot, Resources.Load<AudioClip>("Sounds/Gunshot"));
+
+        audioMap.Add(SoundType.GunshotEnemy, "event:/enemy/enemy fire");
+        audioMap.Add(SoundType.Gunshot, "event:/player/weapons");
+
     }
 
-    void PlayClipAt(AudioClip clip, Vector3 pos, float volume)
+    void PlayClipAt(string path, Vector3 pos, float volume)
     {
-        Sound sound;
-        if (soundPool.soundPool.TryInstantiate(out sound, pos, Quaternion.identity))
-        {
-            sound.Init(clip, volume);
-        }
-        //GameObject tempGO = new GameObject("TempAudio"); // create the temp object
-        //tempGO.transform.position = pos; // set its position
-        //AudioSource aSource = tempGO.AddComponent<AudioSource>(); // add an audio source
-        //aSource.clip = clip; // define the clip
-        //// set other aSource properties here, if desired
-        //aSource.volume = volume;
-        //aSource.dopplerLevel = 0;
-        //aSource.spatialBlend = 1;
-        //aSource.Play(); // start the sound
-        //Destroy(tempGO, clip.length); // destroy object after clip duration
-        //return aSource; // return the AudioSource reference
+        //Sound sound;
+        //if (soundPool.soundPool.TryInstantiate(out sound, pos, Quaternion.identity))
+        //{
+        //    sound.Init(clip, volume);
+        //}
+
+        FMODUnity.RuntimeManager.PlayOneShot(path, pos);
     }
 
 
